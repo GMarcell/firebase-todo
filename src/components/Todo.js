08e903c52327e365e0543f59981b03607f239f8d
GMarcell <grand1310marcell@gmail.com>
2022-01-16 15:26:56 +0700

    Fix Input (Clear after submit)

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,21 +1,24 @@
-import { Box, ListItem, Typography } from '@mui/material'
-import { deleteDoc, doc, getDoc } from 'firebase/firestore'
+import { Box, ListItem, Typography, IconButton } from '@mui/material'
+import { deleteDoc, doc } from 'firebase/firestore'
 import React from 'react'
 import db from '../firebase'
+import CloseIcon from '@mui/icons-material/Close';
 
 function Todo({ todo, deadline, iddoc }) {
     const deleteTask = async (id) => {
-        console.log(id);
+        // console.log(id);
         await deleteDoc(doc(db, "todo", id))
     }
     return (
-        <Box sx={{ borderRadius: 1, border: 5, display: "flex", borderRadius: 16, mt: 2 }} >
+        <Box sx={{ border: 5, display: "flex", borderRadius: 16, mt: 2 }} >
             <ListItem style={{ display: "flex" }}>
                 <Box justifyContent="center" alignItems="center" >
                     <Typography variant="body1" > ✅ Task: {todo}</Typography>
                     <Typography variant="body2" > ⏰ Deadline: {deadline}</Typography>
-                    <button onClick={() => { deleteTask(iddoc) }}>HAPUS</button>
                 </Box>
+                <IconButton onClick={() => { deleteTask(iddoc) }}>
+                    <CloseIcon />
+                </IconButton>
             </ListItem>
         </Box >
     )
--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -8,19 +8,36 @@ import "../App.css"
 function TodoForm() {
     const dbTodoRef = collection(db, "todo")
     const [newTask, setnewTask] = useState("")
+    const [taskInput, settaskInput] = useState("")
     const [newDeadline, setnewDeadline] = useState("")
+    const [deadlineInput, setdeadlineInput] = useState("")
     const addTask = async (e) => {
         e.preventDefault()
         await addDoc(dbTodoRef, { task: newTask, deadline: newDeadline })
         // console.log(newDeadline);
+        setnewTask("")
+        setnewDeadline("")
     }
     return (
         <form onSubmit={addTask} className='todo-form' >
             <div className='inputs'>
-                <TextField sx={{ m: 0.5, p: 0 }} className='task-input' size="small" variant="filled" placeholder='Task...' onChange={(event) => { setnewTask(event.target.value) }} />
-                <TextField sx={{ m: 0.5, p: 0 }} className="date-input" type="date" variant="filled" size="small" onChange={(event) => { setnewDeadline(event.target.value) }} />
+                <TextField
+                    sx={{ m: 0.5, p: 0 }}
+                    className='task-input'
+                    size="small" variant="filled"
+                    placeholder='Task...'
+                    onChange={(event) => { setnewTask(event.target.value) }}
+                    value={newTask} />
+                <TextField
+                    sx={{ m: 0.5, p: 0 }}
+                    className="date-input"
+                    type="date"
+                    variant="filled"
+                    size="small"
+                    onChange={(event) => { setnewDeadline(event.target.value) }}
+                    value={newDeadline} />
             </div>
-            <Button sx={{ m: 2 }} variant="contained" type='submit'>SUBMIT</Button>
+            <Button disabled={!newTask && !newDeadline} sx={{ m: 2 }} variant="contained" type='submit'>SUBMIT</Button>
         </form>
     )
 }
